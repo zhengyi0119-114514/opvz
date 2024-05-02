2c230f260761346abf41e16a97426c0fea1fd8dd
zhengyi0119 <zhengyi0119@outlook.com>
2024-05-02 20:25:35 +0800

    2024/5/2,20:25:30

--- main.cpp
+++ main.cpp
@@ -1,8 +1,15 @@
 #define __STDC_WANT_LIB_EXT1__ 1
 #include <stdio.h>
-
+#include <string.h>
+#include "Information.h"
 
 int main (int agvc,char** agvs)
 {
-    
+    if((agvc > 1 && (!strcmp(agvs[1],"-v")||!strcmp(agvs[1],"--version")))||true)
+    {
+        wprintf_s(L"project:%ls version:%ls "
+            "buildtime:%ls ",
+            PROJECT_NAME_W,PROJECT_VERSION_W,PROJECT_BUILD_TIME_W);
+        return 0;
+    }
 }
